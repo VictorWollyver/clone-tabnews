e259648c18df2e4e5e3b0fa4d48d905d8e1e04a0
VictorWollyver <victorwollyver50@gmail.com>
2025-01-18 17:52:50 -0300

    Adicionado o nome de forma dinamica por searchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-const Index = () => {
+const Index = async ({
+	searchParams,
+}: {
+	searchParams: { [key: string]: string | string[] | undefined };
+}) => {
+	const name = searchParams.name as string;
+	console.log(name);
 	return (
 		<div className="grid place-items-center h-screen">
 			<h1 className="text-5xl font-bold animate-bounce cursor-pointer">
-				ANDRESSA
+				{name}
 			</h1>
 		</div>
 	);
