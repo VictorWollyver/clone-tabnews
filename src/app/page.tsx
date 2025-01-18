26060b6bfac7258bd972aacd87d7acff1bae8428
VictorWollyver <victorwollyver50@gmail.com>
2025-01-18 18:24:54 -0300

    Fix: corrigido problema com searchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,8 @@ import React from "react";
 
 const Index = async ({
 	searchParams,
-}: {
-	searchParams: { [key: string]: string | string[] | undefined };
-}) => {
-	const name = searchParams.name as string;
-	console.log(name);
+}: { searchParams: Promise<{ name: string }> }) => {
+	const { name } = await searchParams;
 	return (
 		<div className="grid place-items-center h-screen">
 			<h1 className="text-5xl font-bold animate-bounce cursor-pointer">
