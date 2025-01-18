feed42d9f6620fce2b386506f63a76801c045a73
VictorWollyver <victorwollyver50@gmail.com>
2025-01-18 18:59:51 -0300

    Feat: adicionado troca de cores ao nome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-import React from "react";
+import NameAnimation from "@/components/NameAnimation";
 
 const Index = async ({
 	searchParams,
 }: { searchParams: Promise<{ name: string }> }) => {
 	const { name } = await searchParams;
+
 	return (
 		<div className="grid place-items-center h-screen">
-			<h1 className="text-5xl font-bold animate-bounce cursor-pointer">
-				{name}
-			</h1>
+			<NameAnimation name={name} />
 		</div>
 	);
 };
---
+++ src/components/NameAnimation.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+
+const colors = [
+	"text-red-500",
+	"text-blue-500",
+	"text-green-500",
+	"text-yellow-500",
+	"text-purple-500",
+	"text-pink-500",
+	"text-indigo-500",
+	"text-gray-500",
+	"text-white",
+  "text-rose-950",
+];
+
+const NameAnimation = ({ name }: { name: string }) => {
+	const [color, setColor] = useState(colors[0]);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setColor(colors[Math.floor(Math.random() * colors.length)]);
+		}, 950);
+		return () => clearInterval(interval);
+	}, []);
+
+	return (
+		<h1 className={`text-5xl font-bold animate-bounce cursor-pointer ${color}`}>
+			{name}
+		</h1>
+	);
+};
+
+export default NameAnimation;
