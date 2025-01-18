9c815a34479cb3ebd3ece9e45332995c0b98c898
VictorWollyver <victorwollyver50@gmail.com>
2025-01-18 17:01:07 -0300

    Fix: removendo props do componente index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-type Props = {};
-
-const Index = (props: Props) => {
+const Index = () => {
 	return (
 		<div className="grid place-items-center h-screen">
 			<h1 className="text-5xl font-bold animate-bounce cursor-pointer">
