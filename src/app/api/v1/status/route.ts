e9d7e2558218ec6625666a71c48fda2565eeea3b
VictorWollyver <victorwollyver50@gmail.com>
2025-01-27 23:33:31 -0400

    Endpoint de `/status` finalizado

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -1,7 +1,57 @@
 import query from "@/infra/database";
 
 export async function GET(request: Request): Promise<Response> {
-	const result = await query("SELECT 1 + 1;");
-	console.log(result);
-	return Response.json({ data: "data" }, { status: 200 });
+	const updatedAt = new Date().toISOString();
+	const databaseName = process.env.POSTGRES_DB;
+	if (!databaseName) {
+		throw new Error("POSTGRES_DB environment variable is not defined");
+	}
+	try {
+		const postgresVersion = await query("SHOW server_version;");
+		const maxConnections = await query("SHOW max_connections;");
+		const usedConnectionsQuery =
+			"SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;";
+		const usedConnections = await query({
+			text: usedConnectionsQuery,
+			values: [databaseName],
+		});
+
+		if (!maxConnections || !postgresVersion || !usedConnections) {
+			throw new Error("Error fetching max connections");
+		}
+
+		const maxConnectionsValue = maxConnections.rows[0].max_connections;
+		const postgresVersionFormatted = postgresVersion.rows[0].server_version;
+		const usedConnectionsCount = usedConnections.rows[0].count;
+
+		return Response.json(
+			{
+				updated_at: updatedAt,
+				dependencies: {
+					database: {
+						postgres_version: postgresVersionFormatted,
+						max_connections: Number(maxConnectionsValue),
+						used_connections: usedConnectionsCount,
+					},
+				},
+			},
+			{ status: 200 },
+		);
+	} catch (error: unknown) {
+		console.error("Error fetching database information", error);
+		if (error instanceof Error) {
+			return Response.json(
+				{
+					error: error.message,
+				},
+				{ status: 500 },
+			);
+		}
+		return Response.json(
+			{
+				error: "An unexpected error occurred",
+			},
+			{ status: 500 },
+		);
+	}
 }
--- src/tests/integrations/api/v1/status/get.test.ts
+++ src/tests/integrations/api/v1/status/get.test.ts
@@ -3,4 +3,16 @@ test("GET to /api/v1/status should return status 200", async () => {
 		method: "GET",
 	});
 	expect(response.status).toBe(200);
+
+	const responseBody = await response.json();
+	console.log(responseBody);
+	expect(responseBody.updated_at).toBeDefined();
+
+	const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+	expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+	expect(responseBody.dependencies.database.postgres_version).toEqual("16.0");
+
+	expect(responseBody.dependencies.database.max_connections).toEqual(100);
+	expect(responseBody.dependencies.database.used_connections).toEqual(1);
 });
